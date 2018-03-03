fa347e56349bc5cd418f98218b0b148e5d837044
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-03-03 09:52:08 -0500

    Small fix to SerializeField syntax name

--- Assets/Game/PlayerName/PlayerName.cs
+++ Assets/Game/PlayerName/PlayerName.cs
@@ -6,7 +6,7 @@ public class PlayerName : MonoBehaviour
 {
 	string _name;
 	NameGenerator nameGenerator;
-	[SerializedField] TextAsset namesJson;
+	[SerializeField] TextAsset namesJson;
 	[SerializeField] TextMeshPro nameText;
 
 	void GenerateNewName ()
