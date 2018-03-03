94f3802a13b9c2dedc09470844d6cdcb80807b04
Franky Dess <dessfrank@hotmail.com>
2018-03-02 21:21:17 -0500

    timer prototype

---
+++ Assets/Timer/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+    [SerializeField]
+    float timeLeft = 30f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timeLeft = Mathf.Round(timeLeft * 100f) / 100f;
+            string timeLeftString = timeLeft.ToString();
+            timeLeftString = timeLeftString.PadRight(5, '0');
+            GetComponent<Text>().text = timeLeftString;
+        }
+        else
+        {
+            timeLeft = 0;
+            timeLeft = Mathf.Round(timeLeft * 100f) / 100f;
+            GetComponent<Text>().text = "" + timeLeft;
+            Debug.Log("EndGame");
+            SceneManager.LoadScene(0);
+        }
+
+    }
+}
