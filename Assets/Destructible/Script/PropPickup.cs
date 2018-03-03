f917af3b8cb5c2434f25f07d6447f395c0587b83
mathieu auclair <mathiass12@hotmail.com>
2018-03-03 05:47:11 -0500

    add propWeapon

---
+++ Assets/Destructible/Script/PropPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropPickup : MonoBehaviour
+{
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.cyan);
+
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 100.0f))
+            print("Found an object - distance: " + hit.distance);
+    }
+}
