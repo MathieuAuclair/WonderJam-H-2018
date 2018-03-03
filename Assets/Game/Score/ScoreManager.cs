bd94e298bcc9dd4ba3e2ae5382f450f41ddfd588
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-03-02 23:24:27 -0500

    Add player name and basic score usage

---
+++ Assets/Game/Score/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent (typeof(Text))]
+public class ScoreManager : MonoBehaviour
+{
+	public static int score;
+
+	[SerializeField] TextMeshPro scoreText;
+
+	void Awake ()
+	{
+		score = 0;
+	}
+
+	void Update ()
+	{
+		scoreText.text = "$" + score + "M";
+	}
+}
