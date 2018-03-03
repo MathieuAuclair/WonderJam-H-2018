d74be85adfd4477fa804c0475728c473961738be
Boud <maxime.boudreault.0@gmail.com>
2018-03-02 23:49:21 -0500

    Support splitscreen

--- Assets/ProCore/ProBuilder/Robot/GameController.cs
+++ Assets/ProCore/ProBuilder/Robot/GameController.cs
@@ -2,7 +2,20 @@
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] Bonfire.Screen screen;
+
     void Start()
     {
+        screen.Initialize();
+        var players = FindObjectsOfType<PlayerController>();
+        foreach (var player in players)
+        {
+            screen.Register(player.transform);
+        }
+    }
+
+    void Update()
+    {
+        screen.Update();
     }
 }
