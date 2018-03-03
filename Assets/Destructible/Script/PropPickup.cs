a8ec6f348d3fc647e23a9db0baff3fc22b9e1ce3
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-03-03 16:32:53 -0500

    WIP: fracture and scene integration

--- Assets/Destructible/Script/PropPickup.cs
+++ Assets/Destructible/Script/PropPickup.cs
@@ -1,24 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PropPickup : MonoBehaviour
 {
-    public float PropPickupDistance = 5.0f;
+	public float PropPickupDistance = 5.0f;
 
-    void FixedUpdate()
-    {
-        RaycastHit hit;
-        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+	void FixedUpdate ()
+	{
+		RaycastHit hit;
+		//Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
 
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, PropPickupDistance))
-        {
+		if (Physics.Raycast (transform.position, Vector3.down, out hit, PropPickupDistance)) {
 
-        }
-    }
+		}
+	}
 
-    private void OnDrawGizmos()
-    {
-        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * PropPickupDistance, Color.green, 0.5f);
-    }
+	void OnDrawGizmos ()
+	{
+		Debug.DrawRay (Camera.main.transform.position, Camera.main.transform.forward * PropPickupDistance, Color.green, 0.5f);
+	}
 }
