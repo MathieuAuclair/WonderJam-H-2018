bb50e87af2fc5358c938c258c8cd416810cfc7de
mathieu auclair <mathiass12@hotmail.com>
2018-03-04 11:25:37 -0500

    WIP first powerup

---
+++ Assets/Game/Robot/PowerUps/BigArm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigArm : RobotActionBase
+{
+    public override void ActionBegins()
+    {
+		gameObject.transform.localScale = gameObject.transform.localScale * 1.4f;
+    }
+
+    public override void ActionEnds()
+    {
+		gameObject.transform.localScale = gameObject.transform.localScale / 1.4f;
+    }
+}
---
+++ Assets/Game/Robot/PowerUps/FastBoost.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastBoost : RobotActionBase
+{
+    public override void ActionBegins()
+    {
+        Rigidbody body = gameObject.GetComponent<Rigidbody>();
+        body.velocity =  body.velocity * 1.5f;
+    }
+
+    public override void ActionEnds()
+    {
+        Rigidbody body = gameObject.GetComponent<Rigidbody>();
+        body.velocity =  body.velocity / 1.5f;
+    }
+}
