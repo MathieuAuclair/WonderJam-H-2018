101e372b421ca712a053eaaa757d3e8ae9be4853
Raphaël Côté <cotlarrc@gmail.com>
2018-03-03 23:06:09 -0500

    Ajout de l'abstract pour faire des powerups

---
+++ Assets/Game/Robot/PowerUps/RobotActionBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace AssemblyCSharp
+{
+	public abstract class RobotActionBase : MonoBehaviour
+	{
+		public abstract void ActionBegins ();
+
+		public abstract void ActionEnds ();
+	}
+}
