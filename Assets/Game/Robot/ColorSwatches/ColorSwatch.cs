df10ae87301a98482181bfe4f933311ae6a02a6e
Boud <maxime.boudreault.0@gmail.com>
2018-03-04 07:50:19 -0500

    Add color swatch scriptable object

---
+++ Assets/Game/Robot/ColorSwatches/ColorSwatch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu]
+public class ColorSwatch : ScriptableObject
+{
+    public enum Part
+    {
+        LIMB,
+        JOINT,
+        BODY,
+        GLASS,
+    }
+
+    [SerializeField] Material limb;
+    [SerializeField] Material joint;
+    [SerializeField] Material body;
+    [SerializeField] Material glass;
+
+    IDictionary<Part, Material> swatch;
+
+    public void Initialize()
+    {
+        swatch = new Dictionary<Part, Material>(4)
+        {
+            { Part.LIMB, limb },
+            { Part.JOINT, joint },
+            { Part.BODY, body },
+            { Part.GLASS, glass },
+        };
+    }
+
+    public void Paint(MeshRenderer part, Part color)
+    {
+        part.material = swatch[color];
+    }
+}
