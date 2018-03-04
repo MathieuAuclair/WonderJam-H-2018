4154b1f3b8425283a5c27a0e5778047e640c52cb
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-03-04 10:23:48 -0500

    Reindent GameOutput

--- Assets/LeaderBoard/GameOutput.cs
+++ Assets/LeaderBoard/GameOutput.cs
@@ -1,31 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class GameOutput : MonoBehaviour
 {
+	Text displayUiText;
 
-    Text displayUiText;
+	string output = PlayerPrefs.GetString ("Score");
 
-    string output = PlayerPrefs.GetString("Score");
-
-    void Start()
-    {
-		displayUiText = gameObject.GetComponent<Text>();
+	void Start ()
+	{
+		displayUiText = gameObject.GetComponent<Text> ();
  
-        foreach (string tagName in ScoreBoard.GetScores().Keys)
-        {
-            output += tagName + " - " + ScoreBoard.GetScores()[tagName] + "M$\n";
+		foreach (string tagName in ScoreBoard.GetScores().Keys) {
+			output += tagName + " - " + ScoreBoard.GetScores () [tagName] + "M$\n";
 		}
 
-        displayUiText.text = output;
-        PlayerPrefs.SetString("Score", output);
-    }
+		displayUiText.text = output;
+		PlayerPrefs.SetString ("Score", output);
+	}
 }
 
 public class PlayerScore
 {
-    public string Name;
-    public int Score;
+	public string Name;
+	public int Score;
 }
