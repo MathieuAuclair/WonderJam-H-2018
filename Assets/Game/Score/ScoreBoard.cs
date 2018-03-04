2e84f7a6407e5bb4f58822da07309c7a78d5b535
Raphaël Côté <cotlarrc@gmail.com>
2018-03-03 21:00:53 -0500

    Created scoreboard basics

---
+++ Assets/Game/Score/ScoreBoard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+public partial class ScoreBoard
+{
+	readonly IDictionary<string, int> scores = new Dictionary<string, int>();
+	private string maxScoreKey = "";
+
+	public IDictionary<string, int> GetScores()
+	{
+		return scores;
+	}
+
+	public void Reset()
+	{
+		scores.Clear();
+	}
+
+	public void IncreaseScore(string id, int gain)
+	{
+		if (scores.ContainsKey(id))
+			scores[id] += gain;
+		else
+			scores[id] = gain;
+
+		if (scores [id] > scores[maxScoreKey]) {
+			maxScoreKey = id;
+		}
+	}
+
+	public string GetLeading(){
+		return maxScoreKey;
+	}
+}
