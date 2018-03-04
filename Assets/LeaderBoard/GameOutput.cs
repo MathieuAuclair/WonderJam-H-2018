e7b9902f079d803f8b91ca2d7a065da450b382ce
mathieu auclair <mathiass12@hotmail.com>
2018-03-04 04:58:37 -0500

    adding a leaderboard that load stat from static ScoreBord

---
+++ Assets/LeaderBoard/GameOutput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOutput : MonoBehaviour
+{
+
+    Text displayUiText;
+
+    string output = PlayerPrefs.GetString("Score");
+
+    void Start()
+    {
+		displayUiText = gameObject.GetComponent<Text>();
+ 
+        foreach (string tagName in ScoreBoard.GetScores().Keys)
+        {
+            output += tagName + " - " + ScoreBoard.GetScores()[tagName] + "M$\n";
+		}
+
+        displayUiText.text = output;
+        PlayerPrefs.SetString("Score", output);
+    }
+}
+
+public class PlayerScore
+{
+    public string Name;
+    public int Score;
+}
