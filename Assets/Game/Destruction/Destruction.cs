efe407f393ffc967da0a9b960ffd2a960989a9cb
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-03-04 10:22:03 -0500

    reindent Destruction.cs

--- Assets/Game/Destruction/Destruction.cs
+++ Assets/Game/Destruction/Destruction.cs
@@ -39,20 +39,20 @@ public class Destruction : MonoBehaviour
 	[Tooltip ("Whether the object makes particles when it Explodes")]
 	public bool particlesOnExplode = false;
 
-        [Space(7)]
-    [Header("Prop Life Span")]
-    [Space(2)]
-    [Tooltip("How much time do you want to keep the destroyed prop")]
-    public float propLifeSpan = 10;
+	[Space (7)]
+	[Header ("Prop Life Span")]
+	[Space (2)]
+	[Tooltip ("How much time do you want to keep the destroyed prop")]
+	public float propLifeSpan = 10;
 
-    [Space(7)]
-    [Header("Is Prop Destroyed After LifeSpan")]
-    [Space(2)]
-    [Tooltip("Whether the object is destroyed or get it's rigidbody disabled")]
-    public bool isPropDestroyedAfterLifeSpan = false;
+	[Space (7)]
+	[Header ("Is Prop Destroyed After LifeSpan")]
+	[Space (2)]
+	[Tooltip ("Whether the object is destroyed or get it's rigidbody disabled")]
+	public bool isPropDestroyedAfterLifeSpan = false;
 
 
-    [SerializeField] const string TAG_ALLOWED_TO_EXPLODE_THINGS = "Player";
+	[SerializeField] const string TAG_ALLOWED_TO_EXPLODE_THINGS = "Player";
 
 	AudioSource src;
 	ParticleSystem particles;
@@ -69,23 +69,20 @@ public class Destruction : MonoBehaviour
 		coll = GetComponent<Collider> ();
 		SetPiecesKinematic (!startBroken);
 
-        if (soundOnExplode)
-        {
-            SetupSound();
-        }
-        if (particlesOnExplode)
-        {
-            SetupParticles();
-        }
-    }
+		if (soundOnExplode) {
+			SetupSound ();
+		}
+		if (particlesOnExplode) {
+			SetupParticles ();
+		}
+	}
 
-    void SetRenderersEnabled(bool value)
-    {
-        for (int i = 1; i < renderers.Length; i++)
-        {
-            renderers[i].enabled = value;
-        }
-    }
+	void SetRenderersEnabled (bool value)
+	{
+		for (int i = 1; i < renderers.Length; i++) {
+			renderers [i].enabled = value;
+		}
+	}
 
 	void SetupSound ()
 	{
@@ -96,90 +93,82 @@ public class Destruction : MonoBehaviour
 		//src.clip = clips [Random.Range (0, clips.Length - 1)];
 	}
 
-    void SetupParticles()
-    {
-        particles = GetComponent<ParticleSystem>();
-        if (particles == null)
-        {
-            particles = gameObject.AddComponent<ParticleSystem>();
-        }
-        particles.Stop();
-    }
+	void SetupParticles ()
+	{
+		particles = GetComponent<ParticleSystem> ();
+		if (particles == null) {
+			particles = gameObject.AddComponent<ParticleSystem> ();
+		}
+		particles.Stop ();
+	}
 
-    void CheckDownwardsForSupports()
-    {
-        if (explodeOnNoSupports)
-        {
-            if (!Physics.Raycast(transform.position, Vector3.down, raycastLength))
-            {
-                ExplodeEverything();
-            }
-        }
-    }
+	void CheckDownwardsForSupports ()
+	{
+		if (explodeOnNoSupports) {
+			if (!Physics.Raycast (transform.position, Vector3.down, raycastLength)) {
+				ExplodeEverything ();
+			}
+		}
+	}
 
 	void OnCollisionEnter (Collision collision)
 	{
 		if (collision.transform.CompareTag (TAG_ALLOWED_TO_EXPLODE_THINGS) && explodeOnCollision) {
 			if (collision.relativeVelocity.magnitude > velocityToExplode) {
 				ExplodeEverything ();
-                CrackleAudio.SoundController.PlaySound("destruction");
-                int scream = Random.Range(0, 4);
-                if (scream == 1)
-                {
-                    CrackleAudio.SoundController.PlaySound("scream");
-                }
+				CrackleAudio.SoundController.PlaySound ("destruction");
+				int scream = Random.Range (0, 4);
+				if (scream == 1) {
+					CrackleAudio.SoundController.PlaySound ("scream");
+				}
 				ScoreBoard.IncreaseScore (collision.gameObject.tag, 1);
 			}
 		}
 	}
 
-    public void ExplodeEverything()
-    {
-        SetPiecesKinematic(false);
-        SetRenderersEnabled(true);
-        renderers[0].gameObject.SetActive(false);
-        if (soundOnExplode)
-        {
-            src.Play();
-        }
-        if (particlesOnExplode)
-        {
-            particles.Play();
-        }
-    }
+	public void ExplodeEverything ()
+	{
+		SetPiecesKinematic (false);
+		SetRenderersEnabled (true);
+		renderers [0].gameObject.SetActive (false);
+		if (soundOnExplode) {
+			src.Play ();
+		}
+		if (particlesOnExplode) {
+			particles.Play ();
+		}
+	}
 
-    void SetPiecesKinematic(bool valueIn)
-    {
-        foreach (Rigidbody rigid in rigids)
-        {
-            rigid.isKinematic = valueIn;
-            rigid.GetComponent<Collider>().enabled = !valueIn;
-            if (valueIn == false)
-            {
-                StartCoroutine(WaitToMakeDisabledAfterPropLifeSpan(rigid.gameObject));
-            }
-        }
-        coll.enabled = valueIn;
-        GetComponent<Rigidbody>().isKinematic = !valueIn;
-    }
+	void SetPiecesKinematic (bool valueIn)
+	{
+		foreach (Rigidbody rigid in rigids) {
+			rigid.isKinematic = valueIn;
+			rigid.GetComponent<Collider> ().enabled = !valueIn;
+			if (valueIn == false) {
+				StartCoroutine (WaitToMakeDisabledAfterPropLifeSpan (rigid.gameObject));
+			}
+		}
+		coll.enabled = valueIn;
+		GetComponent<Rigidbody> ().isKinematic = !valueIn;
+	}
 
 
-    private IEnumerator WaitToMakeDisabledAfterPropLifeSpan(GameObject prop)
-    {
-            yield return new WaitForSeconds(propLifeSpan);
-            if(isPropDestroyedAfterLifeSpan){
-                Destroy(prop);
-            } else {
-                prop.GetComponent<Rigidbody>().detectCollisions = false;
-                Destroy(prop, 2.5f);
-            }
-    }
-    public void ExplodeWithForce(float force, float radius = 3)
-    {
-        ExplodeEverything();
-        foreach (Rigidbody rigid in rigids)
-        {
-            rigid.AddExplosionForce(force, transform.position, radius);
-        }
-    }
+	private IEnumerator WaitToMakeDisabledAfterPropLifeSpan (GameObject prop)
+	{
+		yield return new WaitForSeconds (propLifeSpan);
+		if (isPropDestroyedAfterLifeSpan) {
+			Destroy (prop);
+		} else {
+			prop.GetComponent<Rigidbody> ().detectCollisions = false;
+			Destroy (prop, 2.5f);
+		}
+	}
+
+	public void ExplodeWithForce (float force, float radius = 3)
+	{
+		ExplodeEverything ();
+		foreach (Rigidbody rigid in rigids) {
+			rigid.AddExplosionForce (force, transform.position, radius);
+		}
+	}
 }
